36947d25756c42364b23fd7a26ee4a24669e7dfc
Pavel Denisov <pavelsdenisovs1@gmail.com>
2024-11-06 16:27:32 +0200

    Setup CORS

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"messenger-backend/data-access"
 	"messenger-backend/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"time"
 )
 
 func main() {
@@ -16,6 +18,15 @@ func main() {
 
 	// Set up Gin router
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-lenght"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 
 	// Register routes
 	routes.RegisterRoutes(router)
