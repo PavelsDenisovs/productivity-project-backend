96e8d8ee50dd944d99bdab4ef3ad54c9e2597fdb
Pavel Denisov <pavelsdenisovs1@gmail.com>
2025-04-07 20:42:25 +0300

    Add UserID for Note model

--- models/note.go
+++ models/note.go
@@ -7,7 +7,8 @@ import (
 )
 
 type Note struct {
-	ID           uuid.UUID    `json:"id" db:"id"`
+	ID           uuid.UUID `json:"id" db:"id"`
+  UserID       uuid.UUID `json:"user_id" db:"user_id"`
 	Date         string    `json:"date" db:"date"`
 	Content      string    `json:"content" db:"content"`
 	SleepQuality *int      `json:"sleep_quality,omitempty" db:"sleep_quality"`
