91b64b2641f6d65a7a8ea6a5f3ae6f4f847e3427
Pavel Denisov <pavelsdenisovs1@gmail.com>
2025-04-08 21:38:10 +0300

    Add correct formating for date in Note model

--- models/note.go
+++ models/note.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
@@ -9,9 +10,20 @@ import (
 type Note struct {
 	ID           uuid.UUID `json:"id" db:"id"`
   UserID       uuid.UUID `json:"user_id" db:"user_id"`
-	Date         string    `json:"date" db:"date"`
+	Date         time.Time `json:"-" db:"date"`
 	Content      string    `json:"content" db:"content"`
 	SleepQuality *int      `json:"sleep_quality,omitempty" db:"sleep_quality"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
+}
+
+func (n *Note) MarshalJSON() ([]byte, error) {
+	type Alias Note
+	return json.Marshal(&struct {
+		Date string `json:"date"`
+		*Alias
+	}{
+		Date:  n.Date.Format("2006-01-02"),
+		Alias: (*Alias)(n),
+	})
 }
