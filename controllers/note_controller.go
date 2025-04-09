6ee655cf184f5274882ac54231c93f2b891d8235
Pavel Denisov <pavelsdenisovs1@gmail.com>
2025-04-09 19:07:35 +0300

    Correct note controllers responses

--- controllers/note_controller.go
+++ controllers/note_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"productivity-project-backend/models"
 	"productivity-project-backend/services"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -37,7 +38,7 @@ func (nc *noteController) GetAllNotes(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, notes)
+	c.JSON(http.StatusOK, gin.H{"notes": notes})
 }
 
 func (nc *noteController) CreateNote(c *gin.Context) {
@@ -59,12 +60,20 @@ func (nc *noteController) CreateNote(c *gin.Context) {
 		note.Content = ""
 	}
 
+	note.Date = time.Date(
+		note.Date.Year(), 
+		note.Date.Month(), 
+		note.Date.Day(), 
+		0, 0, 0, 0, 
+		time.UTC,
+	)
+
 	if err := nc.noteService.CreateNote(&note); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create note"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
 	}
 
-	c.JSON(http.StatusCreated, note)
+	c.JSON(http.StatusCreated, gin.H{"note": note})
 }
 
 func (nc *noteController) UpdateNote(c *gin.Context) {
@@ -86,5 +95,5 @@ func (nc *noteController) UpdateNote(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, note)
+	c.JSON(http.StatusOK, gin.H{"note": note})
 }
--- models/note.go
+++ models/note.go
@@ -10,7 +10,7 @@ import (
 type Note struct {
 	ID           uuid.UUID `json:"id" db:"id"`
   UserID       uuid.UUID `json:"user_id" db:"user_id"`
-	Date         time.Time `json:"-" db:"date"`
+	Date         time.Time `json:"date" db:"date"`
 	Content      string    `json:"content" db:"content"`
 	SleepQuality *int      `json:"sleep_quality,omitempty" db:"sleep_quality"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
