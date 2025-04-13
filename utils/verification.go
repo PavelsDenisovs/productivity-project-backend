f87cd78199e6a07b6296f17e63ec8b100ad9e04a
Pavel Denisov <pavelsdenisovs1@gmail.com>
2025-04-13 13:13:49 +0300

    Make emails with verif codes not to go to the spam

--- utils/verification.go
+++ utils/verification.go
@@ -3,11 +3,10 @@ package utils
 import (
 	"fmt"
 	"math/rand"
-	"net/smtp"
 	"os"
 	"time"
 
-	
+	"github.com/resend/resend-go/v2"
 )
 
 var randomGenerator = rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -17,28 +16,26 @@ func GenerateVerificationCode() string {
 }
 
 func SendVerificationEmail(email, code string) error {
-	smtpHost := os.Getenv("SMTP_HOST")
-	smtpPort := os.Getenv("SMTP_PORT")
-	smtpUsername := os.Getenv("SMTP_USERNAME")
-	smtpPassword := os.Getenv("SMTP_PASSWORD")
-	senderEmail := os.Getenv("SENDER_EMAIL")
+	apiKey := os.Getenv("RESEND_API_KEY")
+	sender := os.Getenv("SENDER_EMAIL")
 
-	if smtpHost == "" || smtpPort == "" || smtpUsername == "" || smtpPassword == "" || senderEmail == "" {
-		return fmt.Errorf("missing SMTP credentials in .env file")
+	if apiKey == "" || sender == "" {
+		return fmt.Errorf("missing RESEND_API_KEY or SENDER_EMAIL")
 	}
 
+	client := resend.NewClient(apiKey)
+
 	subject := "Verify Your Email"
-	body := fmt.Sprintf("Your verification code is: %s\n\nThis code will expire in 15 minutes.", code)
+	html := fmt.Sprintf("<p>Your verification code is: <strong>%s</strong></p><p>This code will expire in 15 minutes.</p>", code)
 
-	message := fmt.Sprintf("From: %s\r\n", senderEmail) +
-		fmt.Sprintf("To: %s\r\n", email) +
-		fmt.Sprintf("Subject: %s\r\n", subject) +
-		"\r\n" +
-		body
+	params := &resend.SendEmailRequest{
+		From:    sender,
+		To:      []string{email},
+		Subject: subject,
+		Html:    html,
+	}
 
-	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpHost)
-
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, senderEmail, []string{email}, []byte(message))
+	_, err := client.Emails.Send(params)
 	if err != nil {
 		return fmt.Errorf("failed to send email: %w", err)
 	}
